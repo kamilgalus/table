7876b76d048afb84c9690e019fee8683f53f4ccf
Galus <kamil.galus.pl@gmail.com>
2019-11-15 16:53:22 +0100

    Ustawienie atrybutów jako private

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace TabelaiPionki
+{
+    class Program
+    {
+        static void Main()
+        {
+            Pionek TabelaGlowna = new Pionek();                       //Tworzenie obiektu z którego tabeli będę korzystał ( jedna tabela na której pracuję )     
+            Wyswietlacz PolozeniePionkow = new Wyswietlacz();         //Tworzenie obiektu Wyswietlacza     
+            int pozycjaPionka;                                        //Zmienna do przechowywania indeksu na ktorym użytkownik chce ustawić pionek 
+            bool czyTablicaZapisana;                                  //Zmienna potwierdzająca uzupełnioną tablice
+            int indeksTablicy = 0;                                    //Zmienna która iteruje kolekcje obiektow 
+
+
+            
+            do
+            {
+                Pionek[] TablicaPionkow = new Pionek[5];        //Tablica zawierająca obiekty klasy Pionek            
+                TabelaGlowna.WyswietlTablice(TabelaGlowna);     //Wyswietla tabele  ktora jest wypelniona znakimi '_'                            
+                TablicaPionkow[indeksTablicy] = new Pionek();   //Tworzenie nowego pionka 
+
+                pozycjaPionka = TablicaPionkow[indeksTablicy].PodajPozycjePionka(TabelaGlowna);                 //Pyta gdzie postawic nowego pionka w TabelaGlowna / przypisuje wartosc do pozycjaPionka 
+                TabelaGlowna.UstawPionek(pozycjaPionka, TabelaGlowna, TablicaPionkow[indeksTablicy]);           //Ustawia nowy Pionek  w TabelaGlowna na pozycji 'pozycjaPionka'                
+                PolozeniePionkow.UstawienieKolejnychPionkow(indeksTablicy, pozycjaPionka, PolozeniePionkow);    //Uzupełnia 'tablicaUstawionychPionkow', kolejnym z pionkow na wybranym miejscu 
+
+
+                czyTablicaZapisana = TabelaGlowna.SprawdzTablice(TabelaGlowna);            //Sprawdza czy tablica jest calkowicie uzupelniona pionkami        
+                indeksTablicy++;                                                           //Zmienia indeks tablicy "TablicaPionkow", przmieszczając się miedzy obiektami 
+                Console.Clear();
+            }
+            while (czyTablicaZapisana == false);                                   //Jeśli FALSE to znaczy, że tabela ma jeszcze wolne miejsca na ktorych moze stanac pionek 
+            {
+                Console.Clear();
+                Console.WriteLine("\tTABLICA ZAPISANA");
+                PolozeniePionkow.WyswietlanieKolejnosciPionkow();
+                PolozeniePionkow.WyswietlaniePozycjiPionków(PolozeniePionkow);
+            }
+        }
+
+
+    }
+}
---
+++ Tablica.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace TabelaiPionki
+{
+    abstract class Tablica                    //Klasa abstakcyjna (nie tworze jej obiektów) 
+    {
+        protected char[] tabela = new char[5];
+
+
+        public Tablica()   //Konstruktor wypełniający tablice znakami '_'
+        {
+            for (int i = 0; i < tabela.Length; i++)
+            {
+                tabela[i] = '_';
+            }
+        }
+    
+
+        public void WyswietlTablice(Tablica o)   //Wyswietla TabelaGłowna na ktorej będą lub są ustawione pionki 
+        {
+            Console.Write("\t");
+            for (int i = 0; i < o.tabela.Length; i++)
+            {
+                Console.Write(o.tabela[i]);
+            }
+            Console.WriteLine();
+
+        }
+
+
+
+        public bool SprawdzTablice(Tablica o)  //Funkcja sprawdza czy tablica jest calkowicie obstawiona przez pionki 
+        {
+            int j = 0;                         //Zmienna która będzie przechowywać ilośc miejsc na których są pionki (znak '#')
+
+            for (int i = 0; i < o.tabela.Length; i++)   //Przeszukuje tablice w poszukiwaniu pionków
+            {
+                if (o.tabela[i] == '#')
+                {
+                    j++;
+                }
+            }
+
+            if (j == o.tabela.Length)   //jesli ilosc pionkow w "tabelaOryginalana" jewst równa dlugosci tabeli to znaczy, że na kazdym indeksie stoi pionek 
+            {
+                return true;                 //Zwraca wartość TRUE jeśli tabela jest calkowicie uzupelniona
+            }
+            return false;
+        }
+    }
+}
